acc34526be7aea0989c949da2d2853ebb1c46ef4
Daniek Schuiling <daniekschuiling@gmail.com>
2026-03-10 09:01:12 +0100

    added venv

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-# This tells the server: "When someone visits the home URL (/), run this function"
 @app.route('/')
 def home():
-    # This simulates the data your backend developer will eventually send you!
+    # mock data
     mock_events = [
         {"title": "Frontend Review", "start": "2026-03-12"},
         {"title": "Database Update", "start": "2026-03-15"}
     ]
 
-    # This tells Flask to process your Jinja template and pass the data to it
+    # return to backend
     return render_template('index.html', events_data=mock_events)
 
 
