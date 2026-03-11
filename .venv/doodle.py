72ff63ddcc498af253ac8bc51a101e9e0bd56976
doSosCode <dominik@sostmann.biz>
2026-03-11 13:58:44 +0100

    new logic + app.create_doodle()

--- .venv/doodle.py
+++ .venv/doodle.py
@@ -1,16 +1,24 @@
 from datetime import date, time
 
-def collect_user_dates(
-        date_start = date(2020,2,22),
-        date_end = date(2020,2,22),
-        time_start = time(11,30),
-        time_end = time(16,0),
+def create_attendence():
+    attendence = False
+    # if button = "attend":
+    #   attendence = True
+    return attendence
+
+
+def collect_dates(
+        date_start # = date(2020,2,22),
+        #date_end = date(2020,2,22),
+        #time_start = time(11,30),
+        #time_end = time(16,0),
         ):
-    user_date = (date_start, date_end)
+    user_date = (date_start)
+    doodle_dates.append(user_date)
     return user_date
 
 
-def poll_dates(dates):
+def poll_date(dates):
     polled_dates = []
 
     for date in set(dates):
@@ -22,18 +30,19 @@ def poll_dates(dates):
     return polled_dates
 
 
-user_dates = []
+doodle_dates = []
 
 # Todo:
 # Frontend Daten abgreifen
 # if [Daten Input]:
-user_dates.append(collect_user_dates())
-user_dates.append(collect_user_dates())
-user_dates.append(collect_user_dates())
-user_dates.append(collect_user_dates(date_start=date(2020,2,20),date_end=date(2020,2,20)))
+doodle_dates.append(collect_dates())
+doodle_dates.append(collect_dates())
+doodle_dates.append(collect_dates())
+doodle_dates.append(collect_dates(date_start=date(2020,2,20),date_end=date(2020,2,20)))
 
 
-countedDates = poll_dates(user_dates)
+countedDates = poll_date(doodle_dates)
 countedDates.sort(key=lambda x: x["frequency"], reverse=True)
 
-print(countedDates)
+#for date in countedDates:
+#    print(date)
--- .venv/main.py
+++ .venv/main.py
@@ -1,5 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect
+import doodle
+import queries
+
 app = flask.Flask(__name__)
+
+app.route('/appointments/<appointment_id>', methods=['GET'])
+
+app.route('appointments/<appointment_id>/create_doodle', methods=['POST'])
+def create_doodle(appointment_id):
+    poll_description = request.form['poll_description']
+    poll = queries.create_poll(appointment_id, poll_description)
+    doodle_dates = request.form["dates"]
+    for dateentry in doodle_dates.split(","):
+        queries.create_choice(poll.id, dateentry)
+    return redirect(url_for("appointments", appointments=appointment(id)))
 
 if __name__ == '__main__':
     app.run(debug=True)
