9578aa616314978267d01effc32c2dab0267c153
doSosCode <dominik@sostmann.biz>
2026-03-11 08:14:59 +0100

    working (poll_dates())

--- .venv/doodle.py
+++ .venv/doodle.py
@@ -13,14 +13,12 @@ def collect_user_dates(
 def poll_dates(dates):
     polled_dates = []
 
-    #FUNKTIONIERT NOCH NICHT RICHTIG!!!
     for date in set(dates):
-        #print(date)
         dates_to_poll = {
             "frequency": dates.count(date),
             "date": date
         }
-    polled_dates.append(dates_to_poll)
+        polled_dates.append(dates_to_poll)
     return polled_dates
 
 
@@ -34,10 +32,8 @@ user_dates.append(collect_user_dates())
 user_dates.append(collect_user_dates())
 user_dates.append(collect_user_dates(date_start=date(2020,2,20),date_end=date(2020,2,20)))
 
-for date in user_dates:
-    print(date)
 
 countedDates = poll_dates(user_dates)
-#countedDates.sort(key=lambda x: x["frequency"], reverse=True)
+countedDates.sort(key=lambda x: x["frequency"], reverse=True)
 
 print(countedDates)
