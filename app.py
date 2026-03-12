a3765be781145b184cf371e89699bcdb4972d0f4
Daniek Schuiling <daniekschuiling@gmail.com>
2026-03-12 10:07:04 +0100

    updated app routing in main

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
 # Routing for home
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        events=[],
+        invitations=[],
+        polls=[]
+    )
 
 # Routing for Create Event page
 @app.route("/create_event")
