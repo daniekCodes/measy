03bbcffba92bf566218cc60fc01f4b7c4f938183
Daniek Schuiling <daniekschuiling@gmail.com>
2026-03-12 13:03:56 +0100

    main js functionality written and css added

--- static/main.js
+++ static/main.js
@@ -1,26 +1,35 @@
-// main calendar
-document.addEventListener('DOMContentLoaded', function() {
-    const calendar_element = document.getElementById("calendar")
-    const calendar_config = new FullCalendar.Calendar(calendar_element, {
-        // Standard month view
-        initialView: "dayGridMonth",
-        // Custom buttons
-        customButtons: {
-            add_event_button: {
-                text:"+",
-                click: function() {
-                    window.location.href = "/create_event";
-                }
-            }
-        },
+function location_toggle() {
+    // Check which boxes are assigned to this toggle
+    let physical = document.getElementById("physical_placeholder");
+    let online = document.getElementById("online_placeholder");
 
-        // top menu
-        headerToolbar: {
-            left: "dayGridMonth,timeGridWeek,timeGridDay",
-            center: "title",
-            right: "prev,today,next add_event_button",
-        }
-    })
-    // Render calendar
-    calendar_config.render();
-})
+    // Which radio button is selected
+    let choice = document.querySelector('input[name="location_type"]:checked').value;
+
+    // If/else display choice logic
+    if (choice === 'physical') {
+        physical.style.display = 'block';
+        online.style.display = 'none';
+    } else {
+        physical.style.display = 'none';
+        online.style.display = 'block';
+    }
+}
+
+function meeting_toggle() {
+    // Check which boxes are assigned to this toggle
+    let fixed = document.getElementById("fixed_placeholder");
+    let doodle = document.getElementById("doodle_placeholder");
+
+    // Which radio button is selected
+    let choice = document.querySelector('input[name="meeting_type"]:checked').value;
+
+    // FIX: Changed 'physical' to 'fixed_meeting'
+    if (choice === 'fixed_meeting') {
+        fixed.style.display = 'block';
+        doodle.style.display = 'none';
+    } else {
+        fixed.style.display = 'none';
+        doodle.style.display = 'block';
+    }
+}
